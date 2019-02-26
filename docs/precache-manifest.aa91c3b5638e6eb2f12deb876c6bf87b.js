5d1358b50a95283af6c759fc8f5c685dde3c1096
Samanta Noblea <samantanoblea@gmail.com>
2019-02-26 19:46:21 +0100

    build docs

---
+++ docs/precache-manifest.aa91c3b5638e6eb2f12deb876c6bf87b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "96eadfebd8825174616f",
+    "url": "./static/js/main.96eadfeb.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "./static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "96eadfebd8825174616f",
+    "url": "./static/css/main.0d5e9ea0.chunk.css"
+  },
+  {
+    "revision": "c2d487714174bd8d956afdc8102e4802",
+    "url": "./index.html"
+  }
+];
